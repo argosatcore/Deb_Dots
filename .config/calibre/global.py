403c5fcd1938a5a4add5aa3c23bccc5d8b7a59a0
argosatcore <caguisa@hotmail.com>
2020-12-25 22:48:16 -0600

    Update dots.

--- .config/calibre/global.py
+++ .config/calibre/global.py
@@ -3,95 +3,95 @@
 ### Begin group: DEFAULT
  
 # database path
-# Ruta de la base de datos en la que se almacenan los libros
+# Path to the database in which books are stored
 database_path = '/home/carlos/library1.db'
  
 # filename pattern
-# Patrón para extraer metadatos de los nombres de archivo
+# Pattern to guess metadata from filenames
 filename_pattern = u'(?P<title>.+) - (?P<author>[^_]+)'
  
 # isbndb com key
-# Clave de acceso a isbndb.com
+# Access key for isbndb.com
 isbndb_com_key = ''
  
 # network timeout
-# Tiempo de espera predeterminado para operaciones de red (segundos)
+# Default timeout for network operations (seconds)
 network_timeout = 5
  
 # library path
-# Ruta al directorio en el que está almacenada la biblioteca de libros
+# Path to directory in which your library of books is stored
 library_path = '/home/carlos/Calibre Library'
  
 # language
-# El idioma en que se muestra la interfaz de usuario
-language = 'es'
+# The language in which to display the user interface
+language = 'en'
  
 # output format
-# El formato de salida predeterminado de las conversiones de libros electrónicos.
+# The default output format for e-book conversions.
 output_format = 'EPUB'
  
 # input format order
-# Lista por orden de preferencia de formatos de entrada.
+# Ordered list of formats to prefer for input.
 input_format_order = cPickle.loads('\x80\x02]q\x01(U\x04EPUBq\x02U\x04AZW3q\x03U\x04MOBIq\x04U\x03LITq\x05U\x03PRCq\x06U\x03FB2q\x07U\x04HTMLq\x08U\x03HTMq\tU\x04XHTMq\nU\x05SHTMLq\x0bU\x05XHTMLq\x0cU\x03ZIPq\rU\x04DOCXq\x0eU\x03ODTq\x0fU\x03RTFq\x10U\x03PDFq\x11U\x03TXTq\x12e.')
  
 # read file metadata
-# Leer metadatos desde los archivos
+# Read metadata from files
 read_file_metadata = True
  
 # worker process priority
-# La prioridad de los procesos en ejecución. Una prioridad más alta significa que se ejecutan más rápidamente y consumen más recursos. La mayoría de las tareas, como la conversión, descarga de noticias, adición de libros, etc., se ven afectadas por esta configuración.
+# The priority of worker processes. A higher priority means they run faster and consume more resources. Most tasks like conversion/news download/adding books/etc. are affected by this setting.
 worker_process_priority = 'normal'
  
 # swap author names
-# Intercambiar el nombre y el apellido del autor al leer los metadatos
+# Swap author first and last names when reading metadata
 swap_author_names = False
  
 # add formats to existing
-# Añadir nuevos formatos a los registros de libros existentes
+# Add new formats to existing book records
 add_formats_to_existing = False
  
 # check for dupes on ctl
-# Buscar duplicados al copiar a otra biblioteca
+# Check for duplicates when copying to another library
 check_for_dupes_on_ctl = False
  
 # installation uuid
 # Installation UUID
-installation_uuid = '8b79cbb9-f8d1-497b-95bf-1e5f2cee0407'
+installation_uuid = '2bc6e1a3-4dda-4918-b20f-4e76141169d2'
  
 # new book tags
-# Etiquetas que se aplicarán a los libros que se añadan a la biblioteca
+# Tags to apply to books added to the library
 new_book_tags = cPickle.loads('\x80\x02]q\x01.')
  
 # mark new books
-# Marcar los libros recién añadidos. La marca es temporal y se elimina automáticamente al reiniciar calibre.
+# Mark newly added books. The mark is a temporary mark that is automatically removed when calibre is restarted.
 mark_new_books = False
  
 # saved searches
-# Lista de búsquedas guardadas con nombre
+# List of named saved searches
 saved_searches = cPickle.loads('\x80\x02}q\x01.')
  
 # user categories
-# Categorías del explorador de etiquetas creadas por el usuario
+# User-created Tag browser categories
 user_categories = cPickle.loads('\x80\x02}q\x01.')
  
 # manage device metadata
-# Cómo y cuándo actualiza calibre los metadatos en el dispositivo.
+# How and when calibre updates metadata on the device.
 manage_device_metadata = 'manual'
  
 # limit search columns
-# Cuando se busque texto sin usar prefijos, como «Rojo» en lugar de «title:Rojo», limitar las columnas buscadas a las que se enumeran abajo.
+# When searching for text without using lookup prefixes, as for example, Red instead of title:Red, limit the columns searched to those named below.
 limit_search_columns = False
  
 # limit search columns to
-# Elija las columnas en las que buscar cuando no se usen prefijos, como cuando se busca «Rojo» en lugar de «title:Rojo». Introduzca una lista de nombres de búsqueda separados por comas. Sólo tiene efecto si activa la opción para limitar las columnas de búsqueda más arriba.
+# Choose columns to be searched when not using prefixes, as for example, when searching for Red instead of title:Red. Enter a list of search/lookup names separated by commas. Only takes effect if you set the option to limit search columns above.
 limit_search_columns_to = cPickle.loads('\x80\x02]q\x01(U\x05titleq\x02U\x07authorsq\x03U\x04tagsq\x04U\x06seriesq\x05U\tpublisherq\x06e.')
  
 # use primary find in search
-# Los caracteres introducidos en el cuadro de búsqueda encontrarán también las versiones acentuadas, según el idioma que haya elegido para la interfaz de calibre. Por ejemplo, en inglés, al buscar «n» se encontrará tanto «ñ» como «n», pero en español sólo se encontrará «n». Tenga en cuenta que esto hace que las búsquedas sean mucho más lentas en bibliotecas muy grandes. Además esta opción no tiene efecto si activa la distinción de mayúsculas y minúsculas en las búsquedas
+# Characters typed in the search box will match their accented versions, based on the language you have chosen for the calibre interface. For example, in English, searching for n will match both ñ and n, but if your language is Spanish it will only match n. Note that this is much slower than a simple search on very large libraries. Also, this option will have no effect if you turn on case-sensitive searching
 use_primary_find_in_search = True
  
 # case sensitive
-# Distinguir mayúsculas y minúsculas en las búsquedas
+# Make searches case-sensitive
 case_sensitive = False
  
 # migrated
--- .config/calibre/gui.py
+++ .config/calibre/gui.py
@@ -3,83 +3,83 @@
 ### Begin group: DEFAULT
  
 # send to storage card by default
-# Enviar archivo a tarjeta de almacenamiento en vez de a memoria principal
+# Send file to storage card instead of main memory by default
 send_to_storage_card_by_default = False
  
 # confirm delete
-# Confirmar antes de borrar
+# Confirm before deleting
 confirm_delete = False
  
 # main window geometry
-# Geometría de la ventana principal
+# Main window geometry
 main_window_geometry = cPickle.loads('\x80\x02csip\n_unpickle_type\nq\x01U\x0cPyQt5.QtCoreq\x02U\nQByteArrayU2\x01\xd9\xd0\xcb\x00\x02\x00\x00\x00\x00\x00\n\x00\x00\x00\x1e\x00\x00\x05K\x00\x00\x02\xf5\x00\x00\x00\x0b\x00\x00\x00\x1f\x00\x00\x05J\x00\x00\x02\xf4\x00\x00\x00\x00\x00\x00\x00\x00\x05V\x85\x87Rq\x03.')
  
 # new version notification
-# Notificar cuando haya una nueva versión disponible
+# Notify when a new version is available
 new_version_notification = True
  
 # use roman numerals for series number
-# Usar números romanos para los números de series
+# Use Roman numerals for series number
 use_roman_numerals_for_series_number = True
  
 # sort tags by
-# Ordenar la lista de etiquetas por nombre, popularidad o calificación
+# Sort tags list by name, popularity, or rating
 sort_tags_by = 'name'
  
 # match tags type
-# Buscar un término o todos en las etiquetas
+# Match tags by any or all.
 match_tags_type = 'any'
  
 # cover flow queue length
-# Numero de portadas de libros que se mostrarán en el modo de exploración por portadas
+# Number of covers to show in the cover browsing mode
 cover_flow_queue_length = 6
  
 # LRF conversion defaults
-# Opciones predeterminadas para la conversión a LRF
+# Defaults for conversion to LRF
 LRF_conversion_defaults = cPickle.loads('\x80\x02]q\x01.')
  
 # LRF ebook viewer options
-# Opciones para el visor de libros LRF
+# Options for the LRF e-book viewer
 LRF_ebook_viewer_options = None
  
 # internally viewed formats
-# Formatos que se visualizan usando el visor interno
+# Formats that are viewed using the internal viewer
 internally_viewed_formats = cPickle.loads('\x80\x02]q\x01(U\x03LRFq\x02U\x04EPUBq\x03U\x03LITq\x04U\x04MOBIq\x05U\x03PRCq\x06U\x04POBIq\x07U\x03AZWq\x08U\x04AZW3q\tU\x04HTMLq\nU\x03FB2q\x0bU\x03PDBq\x0cU\x02RBq\rU\x03SNBq\x0eU\x05HTMLZq\x0fU\x05KEPUBq\x10e.')
  
 # column map
-# Columnas a ser mostradas en la lista de libros
+# Columns to be displayed in the book list
 column_map = cPickle.loads('\x80\x02]q\x01(U\x05titleq\x02U\x08ondeviceq\x03U\x07authorsq\x04U\x04sizeq\x05U\ttimestampq\x06U\x06ratingq\x07U\tpublisherq\x08U\x04tagsq\tU\x06seriesq\nU\x07pubdateq\x0be.')
  
 # autolaunch server
-# Ejecutar automáticamente el servidor de contenido al iniciar la aplicación
+# Automatically launch Content server on application startup
 autolaunch_server = False
  
 # oldest news
-# Noticias más antiguas mantenidas en la base de datos
+# Oldest news kept in database
 oldest_news = 60
  
 # systray icon
-# Mostrar icono en el área de notificación del sistema
+# Show system tray icon
 systray_icon = False
  
 # upload news to device
-# Enviar las noticias descargadas al dispositivo
+# Upload downloaded news to device
 upload_news_to_device = True
  
 # delete news from library on upload
-# Borrar los libros nuevos de la biblioteca después de enviarlos al dispositivo
+# Delete news books from library after uploading to device
 delete_news_from_library_on_upload = False
  
 # separate cover flow
-# Mostrar el explorador de portadas en una ventana separada en vez de la ventana principal de calibre
+# Show the cover flow in a separate window instead of in the main calibre window
 separate_cover_flow = False
  
 # disable tray notification
-# Desactivar las notificaciones del icono de la bandeja del sistema
+# Disable notifications from the system tray icon
 disable_tray_notification = False
  
 # default send to device action
-# Acción predeterminada que se ejecutará cuando se pulse el botón de «Enviar al dispositivo»
+# Default action to perform when the "Send to device" button is clicked
 default_send_to_device_action = 'DeviceAction:main::False:False'
  
 # asked library thing password
@@ -87,11 +87,11 @@ default_send_to_device_action = 'DeviceAction:main::False:False'
 asked_library_thing_password = False
  
 # search as you type
-# Ir buscando según se teclea. Si se desactiva esta opción, la búsqueda sólo tendrá lugar cuando se pulse la tecla Intro.
+# Start searching as you type. If this is disabled then search will only take place when the Enter or Return key is pressed.
 search_as_you_type = False
  
 # highlight search matches
-# Al buscar, mostrar todos los libros, resaltando los resultados de la búsqueda, en lugar de mostrar sólo las coincidencias. Puede pulsar la tecla N o la tecla F3 para ir al siguiente resultado.
+# When searching, show all books with search results highlighted instead of showing only the matches. You can use the N or F3 keys to go to the next match.
 highlight_search_matches = False
  
 # save to disk template history
@@ -139,39 +139,39 @@ jobs_search_history = cPickle.loads('\x80\x02]q\x01.')
 tweaks_search_history = cPickle.loads('\x80\x02]q\x01.')
  
 # worker limit
-# Número máximo de tareas de conversión o descarga simultáneas. Este número es el doble del valor real por razones históricas.
+# Maximum number of simultaneous conversion/news download jobs. This number is twice the actual value for historical reasons.
 worker_limit = 6
  
 # get social metadata
-# Descargar metadatos sociales (etiquetas/valoración/etc.)
+# Download social metadata (tags/rating/etc.)
 get_social_metadata = True
  
 # overwrite author title metadata
-# Reemplazar el autor y el título con nuevos metadatos
+# Overwrite author and title with new metadata
 overwrite_author_title_metadata = True
  
 # auto download cover
-# Descargar automáticamente la portada, si estuviera disponible
+# Automatically download the cover, if available
 auto_download_cover = False
  
 # enforce cpu limit
-# Limitar el número máximo de tareas simultáneas al número de CPUs
+# Limit max simultaneous jobs to number of CPUs
 enforce_cpu_limit = True
  
 # gui layout
-# El diseño de la interfaz de usuario. El diseño ancho tiene el panel de detalles del libro a la derecha, el estrecho lo tiene debajo.
+# The layout of the user interface. Wide has the Book details panel on the right and narrow has it at the bottom.
 gui_layout = 'wide'
  
 # show avg rating
-# Mostrar la calificación promedio de cada elemento en el explorador de etiquetas
+# Show the average rating per item indication in the Tag browser
 show_avg_rating = True
  
 # disable animations
-# Desactivar animaciones de la interfaz
+# Disable UI animations
 disable_animations = False
  
 # tag browser hidden categories
-# Categorías del explorador de etiquetas que no se mostrarán
+# tag browser categories not to display
 tag_browser_hidden_categories = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]\x85Rq\x02.')
  
 
