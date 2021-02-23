921164188306f66198a1a46eba856077eefd4962
Monad <caguisa@hotmail.com>
2021-02-22 19:38:48 -0600

    This is enough for me to migrate to awesome. The use of tags is a game changer.

--- .config/awesome/themes/powerarrow-dark/theme.lua
+++ .config/awesome/themes/powerarrow-dark/theme.lua
@@ -106,7 +106,7 @@ local clock = awful.widget.watch(
 theme.cal = lain.widget.cal({
     attach_to = { clock },
     notification_preset = {
-        font = "Terminus 10",
+        font = "FontAwesome 10",
         fg   = theme.fg_normal,
         bg   = theme.bg_normal
     }
