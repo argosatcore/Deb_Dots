7ab0f5751ba68dc17e5695d12bc9f5a7afca5b71
argos <caguisa@hotmail.com>
2021-03-06 02:09:32 -0600

    Script update. Enhanced reproducibility.

--- Debstrap/debstrap.sh
+++ Debstrap/debstrap.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
 #Bootstrapping script for Debian-based systems:
-
+echo 'Initializing bootstrap'
+echo ' '
 echo 'Updating system...'
+echo ' '
 sudo apt update && \
 sudo apt upgrade
 echo ' '
@@ -19,6 +21,7 @@ sudo apt install \
 	deja-dup \
 	feh \
 	ffmpeg \
+	firefox \
 	fonts-font-awesome \
 	fzf \
 	gimp \
@@ -39,14 +42,60 @@ sudo apt install \
 	pavucontrol \
 	picom \
 	redshift \
-	reportbug \
+        reportbug-gtk \
 	rofi \
 	rxvt-unicode \
 	tmux \
+	texlive-full \
 	vim-gtk \
 	vlc \
 	xbacklight  
+sudo remove firefox-esr
 echo ' '
-echo 'Bootstrapping complete.'
-
-
+echo 'Setting up flatpaks...'
+echo ' '
+sudo apt install flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo ' '
+echo 'Installing Zotero...'
+echo ' ' 
+flatpak install flathub org.zotero.Zotero
+echo ' '
+echo 'Capturing dotfiles...'
+echo ' '
+git clone https://github.com/argosatcore/Deb_Dots.git 
+echo ' ' 
+echo 'Deploying  dotfiles.'
+echo ' '
+cd Deb_Dots
+cd Pictures
+mv Wallpapers/ Pictures/
+cd ..
+rm -rf Pictures/
+cd .local
+mv bin/ ~/.local/
+cd share/
+mv fonts/ ~/.local/share/
+mv icons/ ~/.local/share/
+cd ..
+cd ..
+rm -rf .local/
+cd .config/
+sudo mv * ~/.config/
+cd ..
+rm -rf .config/
+mv README.md ~/
+mv .git ~/
+sudo mv .vim ~/
+mv .mouseconfig ~/
+mv .tmux.conf ~/
+sudo mv .vimrc ~/
+mv .Xdefaults ~/
+mv .xsessrionrc ~/
+mv .speedswapper ~/
+sudo mv .gtkrc-2.0
+sudo mv .bashrc ~/
+cd 
+rm -rf Deb_Dots/
+echo ' '
+echo 'Bootstrapping complete. Welcome back, Argos.'
