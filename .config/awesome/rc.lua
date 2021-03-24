460d1786d86b5b9bcf6cf55705aae74ce66fe294
argos <caguisa@hotmail.com>
2021-03-23 23:13:49 -0600

    Added icons to the cascade and cascade.tile layouts.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -111,7 +111,6 @@ awful.layout.layouts = {
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.top,
     lain.layout.centerwork,
-    awful.layout.suit.floating,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
@@ -124,10 +123,11 @@ awful.layout.layouts = {
     --awful.layout.suit.corner.sw,
     --awful.layout.suit.corner.se,
     --lain.layout.cascade,
-    --lain.layout.cascade.tile,
+    lain.layout.cascade.tile,
     --lain.layout.centerwork.horizontal,
     --lain.layout.termfair,
     --lain.layout.termfair.center,
+    awful.layout.suit.floating,
 }
 
 awful.util.taglist_buttons = my_table.join(
@@ -187,9 +187,9 @@ lain.layout.termfair.nmaster           = 3
 lain.layout.termfair.ncol              = 1
 lain.layout.termfair.center.nmaster    = 3
 lain.layout.termfair.center.ncol       = 1
-lain.layout.cascade.tile.offset_x      = dpi(2)
-lain.layout.cascade.tile.offset_y      = dpi(32)
-lain.layout.cascade.tile.extra_padding = dpi(5)
+lain.layout.cascade.tile.offset_x      = dpi(0)
+lain.layout.cascade.tile.offset_y      = dpi(0)
+lain.layout.cascade.tile.extra_padding = dpi(0)
 lain.layout.cascade.tile.nmaster       = 5
 lain.layout.cascade.tile.ncol          = 2
 
