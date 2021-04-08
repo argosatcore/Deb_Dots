5166b70a75b7e52a90b83dee9a82d95fb086d204
argos <caguisa@hotmail.com>
2021-04-08 13:46:34 -0600

    Added a window selector script for wofi.

---
+++ .local/bin/scripts/wofi/window_selector.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get regular windows
+regular_windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
+
+# Get floating windows
+floating_windows=$(swaymsg -t get_tree | jq '.nodes[1].nodes[].floating_nodes[] | (.id|tostring) + " " + .name?'| grep -e "[0-9]* ." | tr -d '"')
+
+enter=$'\n'
+if [[ $regular_windows && $floating_windows ]]; then
+  all_windows="$regular_windows$enter$floating_windows"
+elif [[ $regular_windows ]]; then
+  all_windows=$regular_windows
+else
+  all_windows=$floating_windows
+fi
+
+# Select window with rofi
+selected=$(echo "$all_windows" | wofi -d -i | awk '{print $1}')
+
+# Tell sway to focus said window
+swaymsg [con_id="$selected"] focus
