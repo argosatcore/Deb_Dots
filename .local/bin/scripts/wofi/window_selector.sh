401ffca18e260ca71aadc614e9fe0456523515b5
argosatcore <caguisa@hotmail.com>
2021-05-29 17:42:47 -0600

    Now the window selector can also select scratchpads.

--- .local/bin/scripts/wofi/window_selector.sh
+++ .local/bin/scripts/wofi/window_selector.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Get regular windows
-regular_windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
+# Get available windows
+#windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
 
-# Get floating windows
-floating_windows=$(swaymsg -t get_tree | jq '.nodes[1].nodes[].floating_nodes[] | (.id|tostring) + " " + .name?'| grep -e "[0-9]* ." | tr -d '"')
+windows=$(swaymsg -t get_tree | jq -r '
+	recurse(.nodes[]?) |
+		recurse(.floating_nodes[]?) |
+		select(.type=="con"), select(.type=="floating_con") |
+			(.id | tostring) + " " + .app_id + ": " + .name')
 
-enter=$'\n'
-if [[ $regular_windows && $floating_windows ]]; then
-  all_windows="$regular_windows$enter$floating_windows"
-elif [[ $regular_windows ]]; then
-  all_windows=$regular_windows
-else
-  all_windows=$floating_windows
-fi
 
-# Select window with wofi:
-selected=$(echo "$all_windows" | wofi -d -i | awk '{print $1}')
+# Select window with rofi
+selected=$(echo "$windows" | wofi -d -i -p "Switch to:" | awk '{print $1}')
 
 # Tell sway to focus said window
 swaymsg [con_id="$selected"] focus
