adcc4089128a76aeb96c361c9b225b6c77b5fe79
burrhole <alicesalem93@gmail.com>
2021-06-17 10:04:19 -0400

    changed wofi script to my own

--- .local/bin/scripts/wofi/window_selector.sh
+++ .local/bin/scripts/wofi/window_selector.sh
@@ -5,10 +5,33 @@ windows=$(swaymsg -t get_tree | jq -r '
 	recurse(.nodes[]?) |
 		recurse(.floating_nodes[]?) |
 		select(.type=="con"), select(.type=="floating_con") |
-			(.id | tostring) + " " + .app_id + ": " + .name')
+			" " + .app_id + ": " + .name' | sed '/ $/d;s/\ ://;s/^\ //')
+
+# Find out how many lines wofi needs
+length=$(echo "$windows" | wc -l)
 
 # Select window with wofi
-selected=$(printf "$windows" | wofi -d -i -p "Switch to:" | awk '{print $1}')
+selected=$(printf %s "$windows" | wofi --dmenu --insensitive --lines "$length" -p "Switch to:")
+
+# Make it work with Xwayland or Wayland apps
+case "$selected" in
+	*:\ * )
+		theapp=$(printf '%s' "$selected" | cut -d: -f1 )
+		thename=$(printf '%s' "$selected" | cut -d: -f2 | sed 's/\ //' )
+		identity=$(swaymsg -t get_tree | jq -r '
+			recurse(.nodes[]?) |
+				recurse(.floating_nodes[]?) |
+				select(.app_id=="'"$theapp"'" and .name=="'"$thename"'") |
+					(.id | tostring)')
+		;;
+	* )
+		identity=$(swaymsg -t get_tree | jq -r '
+			recurse(.nodes[]?) |
+				recurse(.floating_nodes[]?) |
+				select(.name=="'"$selected"'") |
+					(.id | tostring)')
+		;;
+esac
 
 # Tell sway to focus said window
-swaymsg [con_id="$selected"] focus
+swaymsg [con_id="$identity"] focus
