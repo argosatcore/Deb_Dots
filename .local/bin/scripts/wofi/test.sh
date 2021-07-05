c33a9f3d68aa59f05811d7421186470ebe1c7536
argosatcore <caguisa@hotmail.com>
2021-07-04 18:09:18 -0600

    Implement calendar notification, ger rid of gsimple cal

---
+++ .local/bin/scripts/wofi/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+swaymsg -t get_tree |
+ jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + " " + .name)' |
+ wofi --show dmenu | {
+   read -r id name
+   swaymsg "[con_id=$id]" focus
+}
