b4a9d761655e2bb0ca4461880d9a7d93ffa6b8aa
argosatcore <caguisa@hotmail.com>
2022-03-12 23:55:30 -0600

    Update scripts

--- .local/bin/scripts/vimwiki/date.sh
+++ .local/bin/scripts/vimwiki/date.sh
@@ -4,8 +4,6 @@ set -e
 cd $HOME/Desktop/vimwiki/Notas/
 nota="$(fdfind -t f -H | fzf --reverse --color=border:#FFFFFF --preview="less {}" --preview-window=:80%:wrap)"
 
-#noteFilename="$HOME/Desktop/vi/note-$(date +%Y-%m-%d).md"
-
 if [ -n $nota ]; then
   echo "# Nota del $(date +%Y-%m-%d)" > $nota
 fi
