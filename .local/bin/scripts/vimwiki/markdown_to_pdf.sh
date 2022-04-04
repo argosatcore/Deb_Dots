8f4308cfc77e190e2d0d80062552e0ef749e9d25
argosatcore <caguisa@hotmail.com>
2022-04-04 00:19:43 -0600

    Less is enough

--- .local/bin/scripts/vimwiki/markdown_to_pdf.sh
+++ .local/bin/scripts/vimwiki/markdown_to_pdf.sh
@@ -6,7 +6,7 @@ doc="$(fdfind -t f -H | fzf --reverse --color=border:#FFFFFF --preview="less {}"
 	--bind="space:toggle-preview" --preview-window=:80%:wrap:hidden)"
 pdftoread="$(echo "${doc%%.*}" | cut -d/ -f2)"
 	if [ -n "$doc" ]; then
-	pandoc "$doc" --pdf-engine=xelatex -V 'fontsize:12pt' --variable monofont="Menlo" -V "geometry:margin=2.54cm" -V 'papersize:letter' -M lang:es -s -o $HOME/Desktop/vimwiki/Pdfs/"$pdftoread".pdf \
+	pandoc "$doc" -V 'fontsize:12pt' --variable monofont="Menlo" -V "geometry:margin=2.54cm" -V 'papersize:letter' -M lang:es -s -o $HOME/Desktop/vimwiki/Pdfs/"$pdftoread".pdf \
 		-F $HOME/.vim/pluged/zotcite/python3/zotref.py -F pandoc-citeproc \
 		--csl=$HOME/Zotero/styles/chicago-fullnote-bibliography.csl
 	fi
