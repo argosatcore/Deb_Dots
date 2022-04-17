4fda78c2e06ea92db598441961599ba38ca9be5a
argosatcore <caguisa@hotmail.com>
2022-04-16 22:48:11 -0600

    Global update

--- .local/bin/Xmobar/Volume/get-volume.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Get the maximum volume of any pulseaudio sink channel
-# amixer get Master | egrep -o "[0-9]+%"
-vol=$(amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1)
-
-echo Vol: $vol%
-
-exit 0
